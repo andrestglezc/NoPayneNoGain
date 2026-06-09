06e9d8ca50b5bfb5ce48b26c1f7697c3e34112f3
Andrés Thomas <andres.t.glez.c@gmail.com>
2026-06-08 22:31:38 -0400

    Initial Commit

---
+++ No Payne No Gain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  No Payne No Gain
+//
+//  Created by Andres.T Glez on 08-06-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ No Payne No Gain/No_Payne_No_GainApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_Payne_No_GainApp.swift
+//  No Payne No Gain
+//
+//  Created by Andres.T Glez on 08-06-26.
+//
+
+import SwiftUI
+
+@main
+struct No_Payne_No_GainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
